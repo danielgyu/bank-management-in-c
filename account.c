4f23ee3eca394d63b08413c066ed4419fc395fb8
kungyu <hismayfly@gmail.com>
2021-06-25 00:54:17 +0900

    account creation complete

--- account.c
+++ account.c
@@ -4,15 +4,17 @@
 
 #include "bank.h"
 
-#define ACCOUNT_LENGTH (8)
+#define NAME_LENGTH (10)
+#define ACCOUNT_LENGTH (9)
+#define PW_LENGTH (10)
 
 struct AccountInfo {
-	char* account_num;
-	char* holder_name;
-	char* password;
+	char holder_name[NAME_LENGTH];
+	char account_num[ACCOUNT_LENGTH];
+	char password[PW_LENGTH];
 };
 
-void generate_number_for_account(size_t count, struct AccountInfo *acc_info)
+void generate_number_for_account(struct AccountInfo* acc_info)
 {
 	int rd1, rd2;
 
@@ -24,20 +26,20 @@ void generate_number_for_account(size_t count, struct AccountInfo *acc_info)
 }
 
 
-struct AccountInfo* create_account(struct AccountInfo *acc_info)
+struct AccountInfo* create_account(struct AccountInfo* acc_info)
 {
-	generate_number_for_account(ACCOUNT_LENGTH, acc_info);
+	generate_number_for_account(acc_info);
 
 	return acc_info;
 }
 
 
-void save_account(struct AccountInfo *acc_info)
+void save_account(struct AccountInfo* acc_info)
 {
 	FILE *fptr;
 	fptr = fopen("account_db", "w+");
 
-	fprintf(fptr, "%s, %s, %s", acc_info->account_num, acc_info->holder_name, acc_info->password);
+	fprintf(fptr, "%s, %s, %s", acc_info->holder_name, acc_info->password, acc_info->account_num);
 
 	fclose(fptr);
 }
@@ -45,7 +47,7 @@ void save_account(struct AccountInfo *acc_info)
 
 int create_new_account()
 {
-	struct AccountInfo *acc_info = malloc(sizeof(struct AccountInfo));
+	struct AccountInfo* acc_info = malloc(sizeof(struct AccountInfo));
 
 	printf("### CREATE ACCOUNT ###\n");
 
@@ -59,7 +61,7 @@ int create_new_account()
 	save_account(acc_info);
 
 	printf("\n");
-	printf("Hello, %s your account number is: %s\n", acc_info->holder_name, acc_info->account_num);
+	printf("Hello %s, your account number is: %s\n", acc_info->holder_name, acc_info->account_num);
 
 	free(acc_info);
 	return 1;	
